3e115b0919023fb97ea250bd5d90a817844d2115
Bruno Tardin <brunotardin20@gmail.com>
2019-03-18 12:39:19 -0300

    Criada rota para salvar novo comunicado

--- webservice/routes/index.js
+++ webservice/routes/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
-var control = require('../controller');
+var express    = require('express');
+var bodyParser = require('body-parser')
+var control    = require('../controller');
 
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 /*
@@ -14,6 +15,15 @@ router.get('/', function(req, res, next) {
 router.get('/resposta', (req, res) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.send(control.getNotificacoesJSON());
+});
+
+/*
+  Recebe o JSON da view responsável por Criar Comunicados
+*/
+router.post('/salvar', function(request, response){
+  console.log(JSON.parse(request.body.json));
+  control.adicionaNotificacao(JSON.parse(request.body.json))
+  response.redirect('/')
 });
 
 module.exports = router;
