fa0090942d90b00cd79a45cde9214a0110e24739
Bruno Tardin <brunotardin20@gmail.com>
2019-03-18 12:31:19 -0300

    Criada rota de resposta para o solicitante (/resposta)

--- webservice/routes/index.js
+++ webservice/routes/index.js
@@ -8,4 +8,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*
+  Envia JSON para o solicitante
+*/
+router.get('/resposta', (req, res) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.send(control.getNotificacoesJSON());
+});
+
 module.exports = router;
