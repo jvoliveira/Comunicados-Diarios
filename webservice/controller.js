be00a9c8d61cbc7a9b9f9c704a2c033faa1a36e5
Bruno Tardin <brunotardin20@gmail.com>
2019-03-21 12:46:15 -0300

    Criado método de validação das datas das notificações.

--- webservice/controller.js
+++ webservice/controller.js
@@ -1,13 +1,58 @@
 const fs = require('fs');
 
+function createObjData(d,h){
+  var data = {
+    dataDia : {
+      dia : d[0],
+      mes : d[1],
+      ano : d[2]
+    },
+    dataHora : {
+      hora : h[0],
+      minutos : h[1]
+    }
+  }
+  return new Date(
+    parseInt(data.dataDia.ano),
+    parseInt(data.dataDia.mes)-1,
+    parseInt(data.dataDia.dia),
+    parseInt(data.dataHora.hora),
+    parseInt(data.dataHora.minutos)
+  )
+}
+
+function notificacaoValida(notificacao) {
+  var d, h;
+  var hoje = new Date();
+  d = notificacao.data.inicio.split(' ')[0].split('/')
+  h = notificacao.data.inicio.split(' ')[1].split(':')
+  var dataInicio = createObjData(d,h)
+  /*
+    Se a notificação não for importante, ela vai expirar
+  */
+  if (!notificacao.importante){
+    d = notificacao.data.fim.split(' ')[0].split('/')
+    h = notificacao.data.fim.split(' ')[1].split(':')
+    var dataFim = createObjData(d,h)
+    if (hoje > dataInicio && hoje < dataFim){
+      return true
+    }
+  } else {
+    if (hoje > dataInicio) return true;
+    return false;
+  }
+}
+
 module.exports.getNotificacoesJSON = function (ip) {
   let rawdata = fs.readFileSync('notifications/conversa.json');
   let obj = JSON.parse(rawdata);
   if (ip != undefined){
     ip = ip.slice(7)
     for (var i = 0; i < obj.notificacao.length; i++) {
-      if (!obj.notificacao[i].recebidos.includes(ip)) {
-        return obj.notificacao[i]
+      if (notificacaoValida(obj.notificacao[i])) {
+        if (!obj.notificacao[i].recebidos.includes(ip)) {
+          return obj.notificacao[i]
+        }
       }
     }
   } else {
